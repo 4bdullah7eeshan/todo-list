50ab8261ccf4d580e8217be876950f3e6086527c
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-09-04 00:50:46 +0530

    wip: add todo properties

--- src/todos.js
+++ src/todos.js
@@ -2,6 +2,12 @@
 
 const createTodo = () => {
     // Factory code
-    
+    const title = '';
+    const description = '';
+    const dueDate = '';
+    const priority = '';
+    const notes = '';
+    const checklist = '';
+    return { title, description, dueDate, priority, notes, checklist }
 }
 
