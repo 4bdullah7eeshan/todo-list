6bc9f65a5bb576ac306d1367907de44a7f533979
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-09-13 16:08:06 +0530

    feat(project btn): add click event listener

--- src/dom.js
+++ src/dom.js
@@ -1,1 +1,7 @@
 // for all dom related stuff
+
+const setUpAddProjectButton = () => {
+    const addProjectButton = document.getElementById('create-new-project-button');
+    addProjectButton.removeEventListener('click', openAddProjectDialog);
+    addProjectButton.addEventListener('click', openAddProjectDialog);
+};
